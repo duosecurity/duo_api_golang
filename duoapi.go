dc8931e0ac63f0383fc8c1960c27dcc5356c9d65
Jonathan Aherne <jaherne@duosecurity.com>
2023-08-24 11:32:55 -0700

    Add new call for sigv5

--- duoapi.go
+++ duoapi.go
@@ -7,11 +7,14 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
 	"io"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/url"
+	"slices"
 	"sort"
 	"strings"
 	"time"
@@ -54,6 +57,42 @@ func canonicalize(method string,
 	return strings.Join(canon[:], "\n")
 }
 
+func canonicalizeV5(method string,
+	host string,
+	uri string,
+	params url.Values,
+	body string,
+	date string) string {
+	var canon [7]string
+	canon[0] = date
+	canon[1] = strings.ToUpper(method)
+	canon[2] = strings.ToLower(host)
+	canon[3] = uri
+	canon[4] = canonParams(params)
+	canon[5] = hashString(body)
+	canon[6] = hashString("") // additional headers not needed at this time
+	return strings.Join(canon[:], "\n")
+}
+
+func hashString(to_hash string) string {
+	hash := sha512.New()
+	hash.Write([]byte(to_hash))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func jsonToValues(json JSONParams) (url.Values, error) {
+	params := url.Values{}
+	for key, val := range json {
+		s, ok := val.(string)
+		if ok {
+			params[key] = []string{s}
+		} else {
+			return nil, errors.New("JSON value not a string")
+		}
+	}
+	return params, nil
+}
+
 func sign(ikey string,
 	skey string,
 	method string,
@@ -62,6 +101,23 @@ func sign(ikey string,
 	date string,
 	params url.Values) string {
 	canon := canonicalize(method, host, uri, params, date)
+	mac := hmac.New(sha512.New, []byte(skey))
+	mac.Write([]byte(canon))
+	sig := hex.EncodeToString(mac.Sum(nil))
+	auth := ikey + ":" + sig
+	return "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func signV5(ikey string,
+	skey string,
+	method string,
+	host string,
+	uri string,
+	date string,
+	params url.Values,
+	body string,
+) string {
+	canon := canonicalizeV5(method, host, uri, params, body, date)
 	mac := hmac.New(sha512.New, []byte(skey))
 	mac.Write([]byte(canon))
 	sig := hex.EncodeToString(mac.Sum(nil))
@@ -133,10 +189,10 @@ func SetTransport(transport func(*http.Transport)) func(*apiOptions) {
 // skey is your Duo integration secret key
 // host is your Duo host
 // userAgent allows you to specify the user agent string used when making
-//           the web request to Duo.  Information about the client will be
-//           appended to the userAgent.
+// the web request to Duo.  Information about the client will be
+// appended to the userAgent.
 // options are optional parameters.  Use SetTimeout() to specify a timeout value
-//         for Rest API calls.  Use SetProxy() to specify proxy settings for Duo API calls.
+// for Rest API calls.  Use SetProxy() to specify proxy settings for Duo API calls.
 //
 // Example: duoapi.NewDuoApi(ikey,skey,host,userAgent,duoapi.SetTimeout(10*time.Second))
 func NewDuoApi(ikey string,
@@ -227,7 +283,7 @@ func (duoapi *DuoApi) SetCustomHTTPClient(c *http.Client) {
 // uri is the URI of the Duo Rest call
 // params HTTP query parameters to include in the call.
 // options Optional parameters.  Use UseTimeout to toggle whether the
-//         Duo Rest API call should timeout or not.
+// Duo Rest API call should timeout or not.
 //
 // Example: duo.Call("GET", "/auth/v2/ping", nil, duoapi.UseTimeout)
 func (duoapi *DuoApi) Call(method string,
@@ -253,7 +309,7 @@ func (duoapi *DuoApi) Call(method string,
 // uri is the URI of the Duo Rest call
 // params HTTP query parameters to include in the call.
 // options Optional parameters.  Use UseTimeout to toggle whether the
-//         Duo Rest API call should timeout or not.
+// Duo Rest API call should timeout or not.
 //
 // Example: duo.SignedCall("GET", "/auth/v2/check", nil, duoapi.UseTimeout)
 func (duoapi *DuoApi) SignedCall(method string,
@@ -282,10 +338,72 @@ func (duoapi *DuoApi) SignedCall(method string,
 	var requestBody io.ReadCloser = nil
 	if method == "POST" || method == "PUT" {
 		headers["Content-Type"] = "application/x-www-form-urlencoded"
-		requestBody = ioutil.NopCloser(strings.NewReader(params.Encode()))
+		requestBody = io.NopCloser(strings.NewReader(params.Encode()))
 	}
 
 	return duoapi.makeRetryableHttpCall(method, url, headers, requestBody, options...)
+}
+
+type JSONParams map[string]any
+
+// Make a signed Duo Rest API call that takes JSON as an argument.
+// See Duo's online documentation for the available REST API's.
+// method is POST or GET
+// uri is the URI of the Duo Rest call
+// json is the JSON parameters to include in the call.
+// options Optional parameters.  Use UseTimeout to toggle whether the
+// Duo Rest API call should timeout or not.
+//
+//	Example:
+//	params := duoapi.JSONParams{
+//		"user_id":         userid,
+//		"activation_code": activationCode,
+//	}
+//	JSONSignedCall("POST", "/auth/v2/enroll_status", params, duoapi.UseTimeout)
+func (duoapi *DuoApi) JSONSignedCall(method string,
+	uri string,
+	params JSONParams,
+	options ...DuoApiOption) (*http.Response, []byte, error) {
+
+	body_methods := []string{"POST", "PUT", "PATCH"}
+	params_go_in_body := slices.Contains(body_methods, method)
+
+	now := time.Now().UTC().Format(time.RFC1123Z)
+	var body string
+	api_url := url.URL{
+		Scheme: "https",
+		Host:   duoapi.host,
+		Path:   uri,
+	}
+
+	url_values := url.Values{}
+	if params_go_in_body {
+		body_bytes, _ := json.Marshal(params)
+		body = string(body_bytes[:])
+	} else {
+		body = ""
+		var err error
+		url_values, err = jsonToValues(params)
+		if err == nil {
+			api_url.RawQuery = url_values.Encode()
+		} else {
+			return nil, nil, err
+		}
+	}
+	auth_sig := signV5(duoapi.ikey, duoapi.skey, method, duoapi.host, uri, now, url_values, body)
+
+	method = strings.ToUpper(method)
+	headers := make(map[string]string)
+	headers["User-Agent"] = duoapi.userAgent
+	headers["Authorization"] = auth_sig
+	headers["Date"] = now
+	var requestBody io.ReadCloser = nil
+	if params_go_in_body {
+		headers["Content-Type"] = "application/json"
+		requestBody = io.NopCloser(strings.NewReader(body))
+	}
+
+	return duoapi.makeRetryableHttpCall(method, api_url, headers, requestBody, options...)
 }
 
 func (duoapi *DuoApi) makeRetryableHttpCall(
